384dd191f98e8931e4169907b4fd3209af12f213
DELL <DELL@DESKTOP-RLFFAEP.bbrouter>
2023-05-08 11:48:15 +0530

    test case2 Added

---
+++ orangehrm/src/testcases/Testcase2.java
@@ -0,0 +1,11 @@
+package testcases;
+
+public class Testcase2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is Test case 2");
+
+	}
+
+}
