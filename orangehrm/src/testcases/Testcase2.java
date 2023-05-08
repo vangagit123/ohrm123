91d1dd13a68b56d2ec3f8a90e0f8168af8445019
DELL <DELL@DESKTOP-RLFFAEP.bbrouter>
2023-05-08 13:16:42 +0530

    testcase modified2

--- orangehrm/src/testcases/Testcase2.java
+++ orangehrm/src/testcases/Testcase2.java
@@ -5,6 +5,7 @@ public class Testcase2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("this is Test case 2");
+		System.out.println("Test case 2 Modified");
 
 	}
 
